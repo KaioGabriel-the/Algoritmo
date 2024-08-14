75bafab7e88f22b3f5ef1a5c0edc6bd3cc2640cd
Your Name <you@example.com>
2024-08-14 17:44:14 -0300

    feat: modificações da questão

--- Pense_python/desafio_string.py
+++ Pense_python/desafio_string.py
@@ -1,30 +1,4 @@
-def possui_letra(palavra,conj_letras):
-
-    for letra in palavra:
-
-        if letra in conj_letras:
-            return True
-    
-    return False
-
-def split_manual(string):
-
-    list_proibida = []
-
-    for chr in string:
-
-        if chr == "," or chr == " ":
-            True
-        else:
-            list_proibida.append(chr)
-
-    return list_proibida
-
-def main():
-
-    menu =f'       -----MENU----- \n 0- ENCERRAR O PROGRAMA \n 1- PALAVRAS COM MAIOR OU IGUAL A \"N\" LETRAS \n 2- PALAVRAS COM MENOS OU IGUAL A \"N\" LETRAS \n 3- PALAVRAS COM QUANTIDADE DE \"N\" DE LETRAS \n 4- PALAVRAS QUE TENHAM A LETRA \"N\" \n 5- PALAVRAS QUE NÃO TENHAM A LETRA \"N\" 6- LETRAS PROIBIDAS \n'
-    print(menu)
-    estado = int(input("QUAL OPÇÃO:"))
+def opcoes(estado,menu):
 
     while estado > 0:
         fin = open('words.txt')
@@ -33,14 +7,8 @@ def main():
 
             qtd_chr = int(input('QUAL A QUANTIDADE DE LETRAS: '))
 
-            for line in fin:
-                word = line.strip()
-                    
-                if len(word) >= qtd_chr:
-                    print(word)
-                    
-            fin.close()
-            
+            palavras_maior = maoir_igual_ou_menor_igual(qtd_chr,fin,1)
+
             print(menu)
 
             estado = int(input("QUAL OPÇÃO:"))
@@ -49,12 +17,8 @@ def main():
 
             qtd_chr = int(input('QUAL A QUANTIDADE DE LETRAS: '))
 
-            for line in fin:
-                word = line.strip()
-                    
-                if len(word) <= qtd_chr:
-                    print(word)
-                    
+            palavras_menor = maoir_igual_ou_menor_igual(qtd_chr,fin,2)
+
             fin.close()
             
             print(menu)
@@ -151,19 +115,63 @@ def main():
                 if not possui_letra(line,list_proibidas):
                     qtd_pl+= 1
                 
-            print(qtd_pl)
+            
+            resultado = f'{qtd_pl}'
 
             fin.close()
-
-            porcentagem = (len(plv_s_n)/len(plv_n)) * 100
-
-            resultado = f'PORCENTAGEM DE PALAVRAS QUE NÃO TENHAM A LETRA {n} \n {porcentagem:.2f}%'
-
-            print(resultado)
 
             print(menu)
 
             estado = int(input("QUAL OPÇÃO:"))
 
+        elif estado == 7:
+            pass
+
+def maoir_igual_ou_menor_igual(qtd,fin,n):
+
+    if n == 1: 
+    
+        for line in fin:
+            word = line.strip()
+                
+            if len(word) >= qtd:
+                print(word)
+    elif n == 2:
+
+        for line in fin:
+                word = line.strip()
+                    
+                if len(word) <= qtd:
+                    print(word)
+
+def possui_letra(palavra,conj_letras):
+
+    for letra in palavra:
+
+        if letra in conj_letras:
+            return True
+    
+    return False
+
+def split_manual(string):
+
+    list_proibida = []
+
+    for chr in string:
+
+        if chr == "," or chr == " ":
+            pass
+        else:
+            list_proibida.append(chr)
+
+    return list_proibida
+
+def main():
+
+    menu =f'       -----MENU----- \n 0- ENCERRAR O PROGRAMA \n 1- PALAVRAS COM MAIOR OU IGUAL A \"N\" LETRAS \n 2- PALAVRAS COM MENOS OU IGUAL A \"N\" LETRAS \n 3- PALAVRAS COM QUANTIDADE DE \"N\" DE LETRAS \n 4- PALAVRAS QUE TENHAM A LETRA \"N\" \n 5- PALAVRAS QUE NÃO TENHAM A LETRA \"N\" \n 6- LETRAS PROIBIDAS \n 7- '
+    print(menu)
+    estado = int(input("QUAL OPÇÃO:"))
+
+    excucao = opcoes(estado,menu)
 
 main()
